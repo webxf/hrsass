fd2bde487a32ee2e1a29f32b66e88c9f74a586ae
webxf <j17852533489@163.com>
2022-08-05 11:32:34 +0800

    树形结构

---
+++ src/views/departments/components/treeTools.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-row type="flex" style="width: 100%">
+    <el-col>{{ departs.name }}</el-col>
+    <el-col :span="5">
+      <el-row type="flex">
+        <el-col>{{ departs.manager }}</el-col>
+        <el-col>
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              操作<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <template v-if="!isRoot">
+                <el-dropdown-item>编辑部门</el-dropdown-item>
+                <el-dropdown-item>删除部门</el-dropdown-item>
+              </template>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    departs: {
+      type: Object,
+      required: true,
+    },
+    isRoot: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {}
+  },
+
+  created() {},
+
+  methods: {},
+}
+</script>
+
+<style scoped></style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,20 +1,49 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <tree-tool :departs="company" :isRoot="true"></tree-tool>
+        <el-tree
+          :data="departs"
+          :props="defaultProps"
+          @node-click="handleNodeClick"
+        >
+          <template v-slot="{ data }">
+            <tree-tool :departs="data"></tree-tool>
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import treeTool from './components/treeTools.vue'
 export default {
+  components: {
+    treeTool,
+  },
   data() {
-    return {}
+    return {
+      departs: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' },
+      ],
+      defaultProps: {
+        label: 'name',
+      },
+      company: { name: '总裁办', manager: '张三' },
+    }
   },
 
   created() {},
 
-  methods: {}
+  methods: {
+    handleNodeClick(data) {
+      console.log(data)
+    },
+  },
 }
 </script>
 
