df13bb07467d486ecc81ae5bdfefc2caffa6ebba
webxf <j17852533489@163.com>
2022-08-14 14:55:31 +0800

    完成员工的导入导出

--- src/constant/employees.js
+++ src/constant/employees.js
@@ -422,6 +422,15 @@ export default {
     工号: "workNumber",
     手机号: "mobile",
     转正日期: "correctionTime"
+},
+ exportExcelMapPath : {
+  '手机号': 'mobile',
+  '姓名': 'username',
+  '入职日期': 'timeOfEntry',
+  '聘用形式': 'formOfEmployment',
+  '转正日期': 'correctionTime',
+  '工号': 'workNumber',
+  '部门': 'departmentName'
 }
 }
 
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -10,7 +10,9 @@
             @click="$router.push('/import')"
             >导入</el-button
           >
-          <el-button size="small" type="danger">导出</el-button>
+          <el-button size="small" type="danger" @click="importemploy"
+            >导出</el-button
+          >
           <el-button size="small" type="primary" @click="addFn"
             >新增员工</el-button
           >
@@ -98,6 +100,7 @@
 import { getUserInfoApi, delEmployee } from '@/api/empioyee'
 import employees from '@/constant/employees'
 import addEmployees from './components/addEmployees.vue'
+const { exportExcelMapPath, hireType } = employees
 export default {
   components: {
     addEmployees,
@@ -139,6 +142,34 @@ export default {
     },
     addFn() {
       this.isShow = true
+    },
+    async importemploy() {
+      const { export_json_to_excel } = await import('@/vendor/Export2Excel')
+      const { rows } = await getUserInfoApi({
+        page: 1,
+        size: this.total,
+      })
+
+      const header = Object.keys(exportExcelMapPath)
+      const data = rows.map((item) => {
+        return header.map((h) => {
+          if (h === '聘用形式') {
+            const findItem = hireType.find((hire) => {
+              return hire.id === item[exportExcelMapPath[h]]
+            })
+            return findItem ? findItem.value : '未知'
+          } else {
+            return item[exportExcelMapPath[h]]
+          }
+        })
+      })
+      export_json_to_excel({
+        header, //表头 必填
+        data, //具体数据 必填
+        filename: '员工列表', //非必填
+        autoWidth: true, //非必填
+        bookType: 'xlsx', //非必填
+      })
     },
   },
 }
